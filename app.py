2c9924121273e85521f8e1daa4525b680faf5002
TheZahhak <67797251+TheZahhak@users.noreply.github.com>
2024-09-07 22:42:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ modelo = joblib.load('ml_price_laptop.pkl')
 st.title('Predicción de Precios de Laptops')
 
 # Controles de entrada para las características
-# 'GHz', 'Ram', 'screen_width', 'screen_height','Inches']
+# 'GHz', 'Ram', 'screen_width', 'screen_height']
 ghz = st.number_input('GHz del CPU', min_value=0.1, max_value=5.0, value=2.5)
 ram = st.number_input('RAM (GB)', min_value=1, max_value=64, value=8)
 screen_width = st.number_input('Ancho de Pantalla', min_value=800, max_value=4000, value=1920)
 screen_height = st.number_input('Alto de Pantalla', min_value=600, max_value=3000, value=1080)
-#inches = st.number_input('Inches (GB)', min_value=10.5, max_value=20.5, value=13.3)
 
 type_gaming = st.selectbox('¿Es Gaming?', ['No', 'Sí'])
 type_notebook = st.selectbox('¿Es Notebook?', ['No', 'Sí'])
@@ -30,10 +29,10 @@ type_notebook = 1 if type_notebook == 'Sí' else 0
 # Botón para realizar predicción
 if st.button('Predecir Precio'):
     # Crear DataFrame con las entradas
-    #input_data = pd.DataFrame([[ghz, ram, screen_width, screen_height, inches,  type_gaming, type_notebook]],
-    #                columns=['GHz','Ram', 'screen_width', 'screen_height', 'Inches','TypeName_Gaming', 'TypeName_Notebook'])
-    input_data = pd.DataFrame([[ghz, ram, screen_width, screen_height, inches]],
-                    columns=['GHz','Ram', 'screen_width', 'screen_height', 'Inches'])
+    #input_data = pd.DataFrame([[ghz, ram, screen_width, screen_height,  type_gaming, type_notebook]],
+    #                columns=['GHz','Ram', 'screen_width', 'screen_height', 'TypeName_Gaming', 'TypeName_Notebook'])
+    input_data = pd.DataFrame([[ghz, ram, screen_width, screen_height]],
+                    columns=['GHz','Ram', 'screen_width', 'screen_height'])
 
     # Estandarización de las características
     scaler = StandardScaler()
@@ -44,8 +43,6 @@ if st.button('Predecir Precio'):
 
     # Mostrar predicción
     st.write(f'Precio predecido: {prediction[0]:.2f} euros')
-
-
 
     # Mostrar predicción
     st.write(f'Precio predecido: {prediction[0]:.2f} euros')
